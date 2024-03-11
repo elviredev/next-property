6f29999732e3c77715a4246d9b58fe1976f1ba59
elviredev <sandrine.rodriguez@hotmail.com>
2024-03-11 17:07:07 +0100

    Create layout, homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '@/assets/styles/globals.css'
+
+// Métadonnées - SEO
+export const metadata = {
+    title: 'Next-Property | Trouvez la location idéale',
+    description: 'Trouvez la location de vos rêves',
+    keywords: 'location, propriétés, trouvez locations, trouvez propriétés'
+}
+
+const MainLayout = ({ children }) => {
+    return (
+        <html lang='fr'>
+        <body>
+            <div>{ children }</div>
+        </body>
+        </html>
+    )
+}
+export default MainLayout
+
+
+
+
+
